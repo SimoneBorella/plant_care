cde297fa58de00753b61fdc8115010387af02e24
Borella Simone <borellasimone00@gmail.com>
2025-10-23 23:04:39 +0200

    Uppdate: mongodb found for pi4

--- Application_PlantCare/setup.bash
+++ Application_PlantCare/setup.bash
@@ -4,6 +4,7 @@
 mkdir /srv/mosquitto/config /srv/mosquitto/data /srv/mosquitto/log -p
 cp mosquitto.conf /srv/mosquitto/config/mosquitto.conf
 
-# CouchDB setup
-mkdir /srv/couchdb/data -p
-sudo chown -R 1000:1000 /srv/couchdb/data
+# MongoDB setup
+mkdir -p /srv/mongodb/data
+chown -R 1001:1001 /srv/mongodb/data
+chmod -R 700 /srv/mongodb/data
