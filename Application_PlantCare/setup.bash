1734e762637c3c21a956366b68d39a7dc06dd7ff
Borella Simone <borellasimone00@gmail.com>
2025-10-23 22:51:45 +0200

    Update: couchdb volume change

--- Application_PlantCare/setup.bash
+++ Application_PlantCare/setup.bash
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Mosquitto setup
 mkdir /srv/mosquitto/config /srv/mosquitto/data /srv/mosquitto/log -p
 cp mosquitto.conf /srv/mosquitto/config/mosquitto.conf
+
+# CouchDB setup
+mkdir /srv/couchdb/data -p
+sudo chown -R 1000:1000 /srv/couchdb/data
